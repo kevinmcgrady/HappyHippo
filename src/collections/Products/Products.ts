7db3cdfb3ac2bf88687cbc82b669b22460f1e07e
Kev McGrady <kevinmcgrady47@gmail.com>
2024-01-17 13:38:01 +0000

    feat/schemas

---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,130 @@
+import { CollectionConfig } from 'payload/types';
+
+import { CATEGORIES } from '../../constants/categories';
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {},
+  fields: [
+    {
+      name: 'user',
+      type: 'relationship',
+      relationTo: 'users',
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      label: 'Product details',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'price',
+      label: 'Price in GBP',
+      min: 0,
+      max: 1000,
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'category',
+      label: 'Category',
+      type: 'select',
+      options: CATEGORIES.map(({ label, value }) => ({
+        label,
+        value,
+      })),
+      required: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product file(s)',
+      type: 'relationship',
+      required: true,
+      relationTo: 'product_files',
+      hasMany: false,
+    },
+    {
+      name: 'approved_for_sale',
+      label: 'Product status',
+      type: 'select',
+      access: {
+        create: ({ req }) => req.user.role === 'admin',
+        read: ({ req }) => req.user.role === 'admin',
+        update: ({ req }) => req.user.role === 'admin',
+      },
+      defaultValue: 'pending',
+      options: [
+        {
+          label: 'Pending verfication',
+          value: 'pending',
+        },
+        {
+          label: 'Approved',
+          value: 'approved',
+        },
+        {
+          label: 'Denied',
+          value: 'denied',
+        },
+      ],
+    },
+    {
+      name: 'price_id',
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: 'text',
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: 'stripe_id',
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: 'text',
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: 'images',
+      type: 'array',
+      label: 'Product images',
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: 'Image',
+        plural: 'Images',
+      },
+      fields: [
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+          required: true,
+        },
+      ],
+    },
+  ],
+};
